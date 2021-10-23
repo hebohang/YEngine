1952e70b0d9f93dee981905b47d1758956c49190
hebohang <hebohang1113@gmail.com>
2021-10-23 20:21:46 +0800

    第一次封装，更改结构，龙书移植代码改名为Common

--- YEngine/Common/pch.h
+++ YEngine/Common/pch.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
+
 #include <Windows.h>
 #include <shellapi.h>  // For CommandLineToArgvW
+#include <wrl.h>    // For Microsoft::WRL::ComPtr
 
 // DirectX 12 specific headers.
 #include "d3dx12.h"
@@ -40,3 +43,26 @@
 // Math header files.
 #include "VectorMath.h"
 #include "Common.h"
+
+// My head files
+// #include "d3dUtil.h"
+#include "Helpers.h"
+
+// The min/max macros conflict with like-named member functions.
+// Only use std::min and std::max defined in <algorithm>.
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+//// In order to define a function called CreateWindow, the Windows macro needs to
+//// be undefined.
+//#if defined(CreateWindow)
+//#undef CreateWindow
+//#endif
+
+// using name
+using namespace Microsoft::WRL;
