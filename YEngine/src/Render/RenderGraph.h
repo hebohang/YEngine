5b42acc8788a5792707dfa5babad7a0da49eb3a0
hebohang <hebohang1113@gmail.com>
2021-10-28 22:41:02 +0800

    仍然有很多要完成的选项

---
+++ YEngine/src/Render/RenderGraph.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "SkinnedData.h"
+#include "MathHelper.h"
+#include "SkinnedModel.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+struct RenderItem
+{
+	RenderItem() = default;
+	RenderItem(const RenderItem& rhs) = delete;
+
+	// World matrix of the shape that describes the object's local space
+	// relative to the world space, which defines the position, orientation,
+	// and scale of the object in the world.
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+
+	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+
+	// Dirty flag indicating the object data has changed and we need to update the constant buffer.
+	// Because we have an object cbuffer for each FrameResource, we have to apply the
+	// update to each FrameResource.  Thus, when we modify obect data we should set 
+	// NumFramesDirty = gNumFrameResources so that each frame resource gets the update.
+	int NumFramesDirty = gNumFrameResources;
+
+	// Index into GPU constant buffer corresponding to the ObjectCB for this render item.
+	UINT ObjCBIndex = -1;
+
+	Material* Mat = nullptr;
+	MeshGeometry* Geo = nullptr;
+
+	// Primitive topology.
+	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	// DrawIndexedInstanced parameters.
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	int BaseVertexLocation = 0;
+
+	// Only applicable to skinned render-items.
+	UINT SkinnedCBIndex = -1;
+
+	// nullptr if this render-item is not animated by skinned mesh.
+	SkinnedModelInstance* SkinnedModelInst = nullptr;
+};
+
+enum class RenderLayer : int
+{
+	Opaque = 0,
+	SkinnedOpaque,
+	Debug,
+	Sky,
+	Count
+};
